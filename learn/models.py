f8dd2bb78f6743b52d9ca5f2908fdd8bd16acfe2
bekthat <begzat.kamen@gmail.com>
2024-02-29 11:18:23 +0600

    Add styles

--- learn/models.py
+++ learn/models.py
@@ -32,13 +32,6 @@ class Group(models.Model):
         return self.name
 
 
-# class Access(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accesses')
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='accesses')
-#
-#     def __str__(self):
-#         return f"{self.user} -> {self.product}"
-
 class Access(models.Model):
     class Status(models.TextChoices):
         ACTIVE = 'AC', 'Активен'
